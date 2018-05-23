42ea1c98201480566a82038475bd4e199dd0445d
Dan Mirsky <mirskiy@gmail.com>
2018-05-23 12:44:00 -0400

    Fix typos in activity stream input module

--- bin/input_module_puppet_enterprise_activity_service.py
+++ bin/input_module_puppet_enterprise_activity_service.py
@@ -1,13 +1,9 @@
-
 # encoding = utf-8
 
 import os
 import sys
-import time
-import datetime
 import json
-import jsonpath_rw
-from datetime import datetime
+
 
 def validate_input(helper, definition):
     """Implement your own validation logic to validate the input stanza configurations"""
@@ -16,6 +12,7 @@ def validate_input(helper, definition):
     # port = definition.parameters.get('port', None)
     pass
 
+
 def validate_input(helper, definition):
     """Implement your own validation logic to validate the input stanza configurations"""
     # This example accesses the modular input variable
@@ -23,41 +20,20 @@ def validate_input(helper, definition):
     # port = definition.parameters.get('port', None)
     pass
 
-def collect_events(helper, ew):
-   
-    import datetime
-    import json
-    import jsonpath_rw
-    
-    method = 'GET'
-    api_request = 'application/json' 
-    
-    api_token = helper.get_global_setting("api_token")
+
+def get_data(helper, offset):
     server = helper.get_arg('server')
     port = helper.get_arg('port')
     service_id = helper.get_arg('service_id')
-    
-    #get current time
-    now = datetime.datetime.now()
-    
-    #get checkpoint value
-    ckpt = service_id + "_start_time"
-    ckpt_value = helper.get_check_point(ckpt)
+    api_token = helper.get_global_setting("api_token")
 
-    #if there is no checkpoint value - that means its an initial load - set start time to now - 5 Minute
-    if ckpt_value == None:
-        old = now - datetime.timedelta(minutes=5)
-        #format the time
-        # This is a timestamp in UTC-based ISO-8601 format (YYYY-MM-DDThh:mm:ssZ) 
-        start_time = old.strftime("%Y-%m-%dT%H:%M:%SZ") 
-    #if it does exist then checkpoint value is start time
-    else:
-        start_time=ckpt_value
+    method = 'GET'
+    api_request = 'application/json' 
 
-    end_time=now.strftime("%Y-%m-%dT%H:%M:%SZ") 
-
-    
     url = server + ":" + port + "/activity-api/v1/events?service_id=" + service_id
+    url += "&limit=10"  # TODO for debugging
+    if offset is not None:
+        url += "&offset={}".format(offset)
     
     headers = {
            'X-Authentication': api_token, 
@@ -77,25 +53,79 @@ def collect_events(helper, ew):
      
     r_status = response.status_code
     response.raise_for_status()
-    helper.log_error (response.text) 
+    helper.log_error(response.text) 
     
-    r= response.json()
-     
+    return response.json()
+
+
+def write_events(helper, ew, events_data):
+    service_id = helper.get_arg('service_id')
     input_type = helper.get_input_type()
+
     for stanza_name in helper.get_input_stanza_names():
+        for one_dict in events_data:
+            one_dict['_service_id'] = service_id
+            data = json.dumps(one_dict, sort_keys=False)
         
-        for one_dict in r['commits']:
-            data = json.dumps(one_dict,sort_keys=False)
-            data['_service_id'] = service_id
-        
-            event = helper.new_event(source=input_type, index=helper.get_output_index(stanza_name), sourcetype=helper .get_sourcetype(stanza_name), data=data)
-            helper.log_error (response.text) 
-            try:
-                ew.write_event(event)
-                helper.log_error (response.text) 
-            except Exception as e:
-                raise e
-        return;
-    
-    #save checkpoint value to end_time which is data collection time
-    ckpt_value = helper.save_check_point(ckpt, end_time)
+            event = helper.new_event(source=input_type, index=helper.get_output_index(stanza_name), sourcetype=helper.get_sourcetype(stanza_name), data=data)
+            ew.write_event(event)
+        return
+
+
+def collect_events(helper, ew):
+    #get checkpoint value
+    service_id = helper.get_arg('service_id')
+    offset_ckpt_str = service_id + "_offset"
+    stop_offset_ckpt_str = service_id + "_stop_offset"
+    total_ckpt_str = service_id + "_total"
+
+    offset_ckpt = helper.get_check_point(offset_ckpt_str)  # Will be 0 if we were caught up on previous run
+    stop_offset_ckpt = helper.get_check_point(stop_offset_ckpt_str)  # Only valid if offset_ckpt != 0 or None. Get UP TO this value.
+    previous_total = helper.get_check_point(total_ckpt_str)
+    previous_total = previous_total if previous_total is not None else 0
+
+    # Query first to figure out how many new records there are
+    data = get_data(helper, offset_ckpt)
+    total_rows = data['total-rows']
+    limit = data['limit']
+    new_rows = total_rows - previous_total
+    helper.log_info('Got {} new rows. Previous total {}, current total {}'.format(new_rows, previous_total, total_rows))
+    helper.save_check_point(total_ckpt_str, total_rows)
+
+    if offset_ckpt is None or offset_ckpt == 0:  # We are getting new data
+        if new_rows <= limit:  # We can get all
+            events_data = data['commits'][:new_rows]
+            helper.save_check_point(offset_ckpt_str, 0)
+        else:  # We need to catch up on the next run
+            events_data = data['commits']
+            helper.save_check_point(offset_ckpt_str, limit)
+            helper.save_check_point(stop_offset_ckpt_str, new_rows)
+    else:  # We are still catching up, want to get up to stop_offset_ckpt
+        rows_to_get = stop_offset_ckpt - offset_ckpt
+        if new_rows > limit:  # We didn't get any new data because it's coming in too fast
+            helper.log_error("Data is coming in too fast. Got {} new rows, but limit is {}!".format(new_rows, limit))
+            events_data = []
+            helper.save_check_point(offset_ckpt_str, offset + new_rows)
+            helper.save_check_point(stop_offset_ckpt_str, new_rows)
+        elif rows_to_get > limit:  # We need to keep catching up on next run
+            events_data = data['commits'][new_rows:]
+            helper.save_check_point(offset_ckpt_str, offset_ckpt + limit)
+            helper.save_check_point(stop_offset_ckpt_str, stop_offset_ckpt + new_rows)
+        else:  # We can finish catching up on this run
+            events_data = data['commits'][new_rows: rows_to_get + new_rows]
+            helper.save_check_point(offset_ckpt_str, 0)
+
+    write_events(helper, ew, events_data)
+
+
+# If offset is not None or 0, we need to get results up to stop_offset_ckpt
+#  ex. offset is 10, stop_offset_ckpt is 15. This means we want records 10 - 15 inclusive
+# We will need to update stop_offset_ckpt based on the total_records in the request and in the checkpoint
+# stop_offset_ckpt += new_total - total_ckpt
+# if offset + limit < stop_offset_ckpt
+#   offset += limit
+# else:
+#   offset = 0, we are back at begining
+# Need to check if we are falling behind too:
+# if new_total - total_ckpt >= limit: falling behind
+# at end: total_ckpt
